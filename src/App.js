4c3a8f8113b070518b959d04ef13d422483a42b0
Jaco Kotzee <jaco@doodlesdigital.co.za>
2020-11-16 11:28:27 +0200

    added searchComponent to render in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Jobs from "./Jobs";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Github Jobs Api</h1>
-      <Jobs name="J Kotzee" title="FE Dev" description="na" />
-      <Jobs name="Pepper" title="BE Dev" description="na" />
-      <Jobs name="Doink" title="Fullstack Dev" description="na" />
+      <SearchParams />
     </div>
   );
 };

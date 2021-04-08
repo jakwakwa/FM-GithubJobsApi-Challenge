be8293fc16529525ea94a41df94a7d07c592d376
Jakwakwa <jaco@doodlesdigital.co.za>
2021-04-08 14:39:48 +0200

    add proxy for cors

--- src/components/Jobs/utils/utils.js
+++ src/components/Jobs/utils/utils.js
@@ -3,24 +3,27 @@ export const urlUpdater = (
   descriptionPropVal,
   fullTimePropVal
 ) => {
-  let url =
-    "https:cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+  const PROXY =
+    window.location.hostname === "localhost"
+      ? "https://cors-anywhere.herokuapp.com"
+      : "/cors-proxy";
+  let url = `${PROXY}/https://jobs.github.com/positions.json`;
 
   if (fullTimePropVal) {
     if (descriptionPropVal.length === 0 && locationPropVal.length > 0) {
-      url = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?&full_time=on&location=${locationPropVal}`;
+      url = `${PROXY}/https://jobs.github.com/positions.json?&full_time=on&location=${locationPropVal}`;
     } else if (locationPropVal.length === 0 && descriptionPropVal.length > 0) {
-      url = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${descriptionPropVal}&full_time=on`;
+      url = `${PROXY}/https://jobs.github.com/positions.json?description=${descriptionPropVal}&full_time=on`;
     } else if (descriptionPropVal.length > 0 && locationPropVal.length > 0) {
-      url = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${descriptionPropVal}&full_time=on&location=${locationPropVal}`;
+      url = `${PROXY}/https://jobs.github.com/positions.json?description=${descriptionPropVal}&full_time=on&location=${locationPropVal}`;
     }
   } else {
     if (descriptionPropVal.length === 0 && locationPropVal.length > 0) {
-      url = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?&location=${locationPropVal}`;
+      url = `${PROXY}/https://jobs.github.com/positions.json?&location=${locationPropVal}`;
     } else if (locationPropVal.length === 0 && descriptionPropVal.length > 0) {
-      url = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${descriptionPropVal}`;
+      url = `${PROXY}/https://jobs.github.com/positions.json?description=${descriptionPropVal}`;
     } else if (descriptionPropVal.length > 0 && locationPropVal.length > 0) {
-      url = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${descriptionPropVal}&location=${locationPropVal}`;
+      url = `${PROXY}/https://jobs.github.com/positions.json?description=${descriptionPropVal}&location=${locationPropVal}`;
     }
   }
 

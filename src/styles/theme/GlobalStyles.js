81ac12cc565d1c0339897a7132a652cd6f6d5abe
Jakwakwa <jaco@doodlesdigital.co.za>
2021-04-17 04:07:09 +0200

    removed commented out code

--- src/styles/theme/GlobalStyles.js
+++ src/styles/theme/GlobalStyles.js
@@ -4,48 +4,6 @@ import { themeColors } from "./ThemeStyled";
 const GlobalStyles = createGlobalStyle`
 
 @import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap');
-${
-  "" /**,
-  *::after,
-  *::before {
-    box-sizing: border-box;
-  }
-
-  body {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-    padding: 0;
-    margin: 0;
-    font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
-    transition: all 0.25s linear;
-  }
-
-  footer {
-    position: absolute;
-    bottom: 5%;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-
-  small {
-    display: block;
-  }
-
-  button {
-    display: block;
-  }
-
-  a {
-    color: ${({ theme }) => theme.text};
-  }*/
-}
-
-
 
 * {
   box-sizing: border-box;

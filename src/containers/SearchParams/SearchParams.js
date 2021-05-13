57b45b5d337dd57c1db96e46c92bc677b736c1d0
Jakwakwa <jaco@doodlesdigital.co.za>
2021-05-13 07:45:06 +0200

    remove comments

--- src/containers/SearchParams/SearchParams.js
+++ src/containers/SearchParams/SearchParams.js
@@ -10,36 +10,8 @@ import LinearProgress from "@material-ui/core/LinearProgress";
 import SearchForm from "../SearchParams/SearchForm";
 import JobCardContainer from "../JobCardContainer/JobCardContainer";
 import { PrimaryButton } from "../../components/Buttons/Buttons";
-//import JobCardFallback from "../../components/JobCard/JobCardFallback";
-// CONSTANTS
-
 import { initialJobsQuery, setQueryDataForJobs } from "../../utils/jobQuery";
 
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-//import { JobForm } from "../pokemon";
-
-//function JobInfo({ jobName }) {
-// 🐨 Have state for the pokemon (null)
-// 🐨 use React.useEffect where the callback should be called whenever the
-// pokemon name changes.
-// 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-// 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-// 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-// 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-//   fetchPokemon('Pikachu').then(
-//     pokemonData => { /* update all the state here */},
-//   )
-// 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-//   1. no pokemonName: 'Submit a pokemon'
-//   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-//   3. pokemon: <PokemonDataView pokemon={pokemon} />
-
-// 💣 remove this
-//  return "TODO";
-//}
 function Jobs({ pageLimit, status, data, handleLoader, disabled }) {
   if (status === "loading") {
     return (

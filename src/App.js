6660af3da69ca60b30d384288b1913bf5ee74fcc
Jaco Kotzee <jakwakwa@gmail.com>
2022-11-29 21:34:42 +0200

    restored all correct package versions

--- src/App.js
+++ src/App.js
@@ -57,7 +57,6 @@ const App = () => {
 
           <Router>
             <SearchParams path="/:query" />
-            {/* <ScrollTop /> */}
             <JobDetails path="/details/:id" />
           </Router>
         </ThemeProvider>
